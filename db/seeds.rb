18f6b197d33d555584cec17c8bb8ef0ca94da7f2
Matt Beruldsen <mberulds@gmail.com>
2023-10-07 17:21:51 +1100

    created required methods, db, pages etc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+10.times do
+  attributes = {
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: category.sample
+  }
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
+
+puts 'Creating reviews...'
+5.times do
+  attributes = {
+    rating: Faker::Number.between(from: 0, to: 5),
+    content: Faker::Restaurant.review
+  }
+  Review.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
